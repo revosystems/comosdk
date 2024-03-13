f27d1fea962bd8cab9a0443695d1310061a8945a
Jordi Puigdellívol <jordi.p@revo.works>
2024-03-13 16:24:36 +0100

    Improved payment flow

--- ComoSdk/src/Controllers/ComoPayController.swift
+++ ComoSdk/src/Controllers/ComoPayController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import RevoFoundation
+import RevoUIComponents
 
 class ComoPayController : UIViewController {
     
@@ -7,14 +8,16 @@ class ComoPayController : UIViewController {
     
     @IBOutlet weak var payCodeInput:UITextField!
     @IBOutlet weak var errorLabel:UILabel!
-    @IBOutlet weak var loading:UIActivityIndicatorView!
+    @IBOutlet weak var loading:LoadingAnimation!
     @IBOutlet weak var amountLabel: UILabel!
-    @IBOutlet var headerImageView: UIView!
-    @IBOutlet var payButton: UIButton!
+    @IBOutlet weak var statusIcon:UIImageView!
+    @IBOutlet weak var pinCode:UIStackView!
+    @IBOutlet weak var titleLabel:UILabel!
+    @IBOutlet weak var descriptionLabel:UILabel!
+    @IBOutlet weak var resendCodeButton:UIButton!
     
-    @IBOutlet private var payTitle: UILabel!
-    @IBOutlet private var payDesc: UILabel!
-    
+    let ERROR_CODE_NEEDS_VERIFICATION = "4007005"
+        
     var delegate:ComoDelegate?
     
     override func viewDidLoad() {
@@ -25,6 +28,9 @@ class ComoPayController : UIViewController {
         
         appearance()
         amountLabel.text = str("%.2f €", Double(amount) / 100.0)
+        
+        loading.set(size: CGSize(width: 50, height: 20), color: .darkGray)
+        startPaymentProcess()
     }
     
     @IBAction func onCancelPressed(_ sender:Any){
@@ -32,34 +38,92 @@ class ComoPayController : UIViewController {
     }
     
     
-    @IBAction func onPayPressed(_ sender:UIButton){
+    private func startPaymentProcess(code:String? = nil){
+        hideAskCodeDetails()
         Task {
             do {
-                loading.start(sender)
+                loading.startAnimating()
                 errorLabel.text = ""
-                let response = try await Como.shared.currentSale!.pay(amount: amount, code: payCodeInput.text)
-                dismiss(animated: true) { [unowned self] in
-                    self.delegate?.como(onPaid: response)
-                }
+                let response = try await Como.shared.currentSale!.pay(amount: amount, code: code)
+                loading.stopAnimating()
+                showPaymentOk(response: response)
             }
             catch {
-                loading.stop(sender)
+                loading.stopAnimating()
+                if("\(error)".contains(ERROR_CODE_NEEDS_VERIFICATION)) {
+                    return askVerificationCode()
+                }
                 errorLabel.text = Como.trans("como_\(error)")
                 print(error)
             }
         }
     }
     
-    func appearance(){
-        headerImageView.circle()
-        payButton.round(4)
+    private func askVerificationCode(){
+        titleLabel.isHidden       = false
+        descriptionLabel.isHidden = false
+        statusIcon.isHidden       = false
+        pinCode.isHidden          = false
+        resendCodeButton.isHidden = false
+    }
+    
+    
+    @IBAction func onResendCodePressed(_ sender:UIButton?){
+        let code = pinCode.arrangedSubviews.compactMap { ($0 as? UITextField)?.text }.implode("")
+        startPaymentProcess(code: code)
+    }
+    
+    private func showPaymentOk(response:Como.PaymentResponse){
+                
+        titleLabel.isHidden         = false
+        titleLabel.text             = Como.trans("como_paymentCompleted")
+        
+        descriptionLabel.isHidden   = false
+        descriptionLabel.text       = Como.trans("como_paymentOk")
+        
+        statusIcon.isHidden         = false
+        statusIcon.image = UIImage(systemName: "checkmark.circle.fill")
+        statusIcon.tintColor = UIColor(hex: "#6BB637")
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) { [unowned self] in
+            dismiss(animated: true) { [unowned self] in
+                self.delegate?.como(onPaid: response)
+            }
+        }
+    }
+    
+    private func hideAskCodeDetails(){
+        titleLabel.isHidden       = true
+        statusIcon.isHidden       = true
+        descriptionLabel.isHidden = true
+        pinCode.isHidden          = true
+        resendCodeButton.isHidden = true
+        errorLabel.text           = ""
+    }
+    
+    private func appearance(){
+        hideAskCodeDetails()
+        
+        descriptionLabel.text = Como.trans("como_paymentCodeHasBeenSentTo") + " " + (Como.shared.currentSale?.customer?.phoneNumber ?? Como.shared.currentSale?.customer?.email ?? "")
     }
     
     func translate(){
-        payTitle.text = Como.trans("como_payTitle")
-        payDesc.text = Como.trans("como_payDesc")
-        payCodeInput.placeholder = Como.trans("como_SMSValidationCode")
-        payButton.setTitle(Como.trans("como_paySendAuthCode"), for: .normal)
+        //payTitle.text = Como.trans("como_payTitle")
+        //payDesc.text = Como.trans("como_payDesc")
+        //payCodeInput.placeholder = Como.trans("como_SMSValidationCode")
+        //payButton.setTitle(Como.trans("como_paySendAuthCode"), for: .normal)
+    }
+    
+    //OTP
+    @IBAction func onPinTextFieldChanged(_ textField:UITextField){
+        let index = pinCode.arrangedSubviews.firstIndex(of: textField) ?? 0
+        (pinCode.arrangedSubviews[index + 1] as? UITextField)?.text = ""
+        (pinCode.arrangedSubviews[index + 1] as? UITextField)?.becomeFirstResponder()
+    }
+    
+    @IBAction func onLastPinTextFieldChanged(_ textField:UITextField){
+        onResendCodePressed(nil)
     }
 }
 
