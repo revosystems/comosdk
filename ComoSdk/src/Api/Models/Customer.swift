b0e2bdf3b9c17a74d7ed2f7fe3bacc282dfe0135
Eduard Altimiras <eduard@revo.works>
2024-10-16 10:50:12 +0200

    Can create customer with more fields

--- ComoSdk/src/Api/Models/Customer.swift
+++ ComoSdk/src/Api/Models/Customer.swift
@@ -4,26 +4,37 @@ extension Como {
     public struct Customer : Codable {
         let phoneNumber:String?
         let email:String?
+        let firstName:String?
+        let lastName:String?
         let appClientId:String?
         let commonExtId:String?
         
+        public init(phone:String?, email:String?, firstName:String?, lastName:String?){
+            self.phoneNumber = phone
+            self.email = email
+            self.firstName = firstName
+            self.lastName = lastName
+            self.appClientId = nil
+            self.commonExtId = nil
+        }
+        
         public init(phoneNumber:String){
-            email = nil; appClientId = nil; commonExtId = nil;
+            email = nil; appClientId = nil; commonExtId = nil; firstName = nil; lastName = nil;
             self.phoneNumber = phoneNumber
         }
         
         public init(email:String){
-            phoneNumber = nil; appClientId = nil;  commonExtId = nil;
+            phoneNumber = nil; appClientId = nil;  commonExtId = nil; firstName = nil; lastName = nil;
             self.email = email
         }
         
         public init(appClientId:String){
-            email = nil; phoneNumber = nil;  commonExtId = nil;
+            email = nil; phoneNumber = nil;  commonExtId = nil; firstName = nil; lastName = nil;
             self.appClientId = appClientId
         }
         
         public init(commonExtId:String){
-            email = nil; phoneNumber = nil;  appClientId = nil;
+            email = nil; phoneNumber = nil;  appClientId = nil; firstName = nil; lastName = nil;
             self.commonExtId = commonExtId
         }
         
