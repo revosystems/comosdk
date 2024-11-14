c61eca5645d07e53755dd3dd33eb9b8310df74f0
Sergi Baró <sergi.baro@revo.works>
2024-11-14 15:34:22 +0100

    added Package.swift

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "ComoSdk",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ComoSdk",
+            targets: ["ComoSdk"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/revosystems/RevoHttp", .upToNextMinor(from: "0.3.9")),
+        .package(url: "https://github.com/revosystems/RevoUIComponents.git", .upToNextMinor(from: "0.1.0"))
+    ],
+    targets: [
+        .target(
+            name: "ComoSdk",
+            dependencies: [
+                "RevoHttp",
+                "RevoUIComponents"
+            ],
+            path: "ComoSdk/src",
+            resources: [
+                .process("ComoSdk/resources")
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
